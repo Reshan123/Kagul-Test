b8a37ccb83a5ea954d4a949a44776271243796f3
Reshan Gomis <reshangomis@gmail.com>
2024-06-25 12:22:55 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ export default function App() {
   return (
     <>
         <div className="header" style={{
+            width: '100%',
+            height: '100vh',
             marginBottom: '500px'
         }}>
             <Spline style={{
                 width: '100%',
                 height: '100vh'
-            }} scene="https://draft.spline.design/QDcBIkkQI5n63r9I/scene.splinecode"/>
+            }} scene="https://draft.spline.design/lFLR4Bv5ifV6BGWa/scene.splinecode"/>
             <h1 style={{
                 position: "absolute",
                 top: '50px',
